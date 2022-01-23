a1c3f8027024a095640ed0af13b3b58518c589a6
NYXTheShadow <alielahirad8087@gmail.com>
2022-01-23 13:56:24 +0330

    Adding bubble sort.

--- SortingAlgorithms/SortingAlgorithms.cs
+++ SortingAlgorithms/SortingAlgorithms.cs
@@ -2,5 +2,21 @@
 
 public class SortingAlgorithms
 {
-    // Sorting Algorithms goes here ...
+    public void BubbleSort(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            bool sorted = true;
+            for (int j = 1; j < list.Count - i; j++)
+            {
+                if (list[j - 1] > list[j])
+                {
+                    (list[j], list[j - 1]) = (list[j - 1], list[j]);
+                    sorted = false;
+                }
+            }
+
+            if (sorted) return;
+        }
+    }
 }
